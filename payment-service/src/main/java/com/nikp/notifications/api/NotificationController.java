6892f7d239e85979809f9e331c8a985e750dfb3a
nikpapag <82569427+nikpapag@users.noreply.github.com>
2021-11-12 10:10:33 +0000

    Update NotificationController.java

--- payment-service/src/main/java/com/nikp/notifications/api/NotificationController.java
+++ payment-service/src/main/java/com/nikp/notifications/api/NotificationController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import com.nikp.notifications.domain.HelloMessage;
 import com.nikp.notifications.domain.PaymentAddedNotification;
 
+
 @Controller
 public class NotificationController {
 
